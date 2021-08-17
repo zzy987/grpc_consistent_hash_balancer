28c3ec76bf7684ba95f05fde4d574370f0a91cf4
zzy987 <67889264+zzy987@users.noreply.github.com>
2021-08-17 15:17:25 +0800

    feat: expand resolver, support updating addresses
    Signed-off-by: zzy987 <67889264+zzy987@users.noreply.github.com>

--- client/resolver.go
+++ client/resolver.go
@@ -9,39 +9,53 @@ import (
 // Following is an example name resolver implementation. Read the name
 // resolution example to learn more about it.
 
+var (
+	Resolver *exampleResolver
+)
+
+func init() {
+	resolver.Register(&exampleResolverBuilder{})
+}
+
 type exampleResolverBuilder struct{}
 
 func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
-	r := &exampleResolver{
+	Resolver = &exampleResolver{
 		target: target,
 		cc:     cc,
-		addrsStore: map[string][]string{
-			serviceName: addrs,
-		},
+		// addrsUpdateChan can be blocked
+		addrsUpdateChan: make(chan []string, 1),
 	}
-	log.Printf("resolver built with addresses %v\n", addrs)
-	r.start()
-	return r, nil
+	Resolver.addrsUpdateChan <- addrs
+	go Resolver.start()
+	return Resolver, nil
 }
 func (*exampleResolverBuilder) Scheme() string { return scheme }
 
 type exampleResolver struct {
-	target     resolver.Target
-	cc         resolver.ClientConn
-	addrsStore map[string][]string
+	target resolver.Target
+	cc     resolver.ClientConn
+	//addrsStore      map[string][]string
+	addrsUpdateChan chan []string
 }
 
 func (r *exampleResolver) start() {
-	addrStrs := r.addrsStore[r.target.Endpoint]
-	addresses := make([]resolver.Address, len(addrStrs))
-	for i, s := range addrStrs {
-		addresses[i] = resolver.Address{Addr: s}
+	for addrStrs := range r.addrsUpdateChan {
+		log.Printf("resolver built with addresses %v\n", addrStrs)
+		addresses := make([]resolver.Address, len(addrStrs))
+		for i, s := range addrStrs {
+			addresses[i] = resolver.Address{Addr: s}
+		}
+		r.cc.UpdateState(resolver.State{Addresses: addresses})
 	}
-	r.cc.UpdateState(resolver.State{Addresses: addresses})
 }
-func (*exampleResolver) ResolveNow(o resolver.ResolveNowOptions) {}
-func (*exampleResolver) Close()                                  {}
 
-func init() {
-	resolver.Register(&exampleResolverBuilder{})
+func (*exampleResolver) ResolveNow(o resolver.ResolveNowOptions) {}
+
+func (r *exampleResolver) Close() {
+	close(r.addrsUpdateChan)
+}
+
+func (r *exampleResolver) UpdateAddress(addrs []string) {
+	r.addrsUpdateChan <- addrs
 }
